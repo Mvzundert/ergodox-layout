309525b332860eb03b3eeb31dfddf15f760323aa
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-05-16 09:59:26 +0200

    Added keyboard layout

---
+++ ergodox_ez_glow_marzun_source/config.h
@@ -0,0 +1,18 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef DEBOUNCE
+#define DEBOUNCE 20
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"z4r9r/EJWNY"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+#define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
